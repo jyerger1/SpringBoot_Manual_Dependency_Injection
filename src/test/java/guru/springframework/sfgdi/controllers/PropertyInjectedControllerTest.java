10f9da7c42d49935b66880d4017d927e2c83de6f
John Yerger <johnyerger@johns-mbp.home>
2020-07-21 13:34:01 -0400

    push

---
+++ src/test/java/guru/springframework/sfgdi/controllers/PropertyInjectedControllerTest.java
@@ -0,0 +1,28 @@
+package guru.springframework.sfgdi.controllers;
+
+import guru.springframework.sfgdi.services.GreetingServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+// least prefered method
+class PropertyInjectedControllerTest {
+
+    PropertyInjectedController controller;
+
+    // new PropertyInjectedController object assigned to variable controller / set
+    // property with new GreetingServiceImpl
+    @BeforeEach
+    void setUp() {
+        controller = new PropertyInjectedController();
+
+        controller.greetingService = new GreetingServiceImpl();
+    }
+
+    @Test
+    void getGreeting() {
+
+        System.out.println(controller.getGreeting());
+    }
+}
---
+++ src/test/java/guru/springframework/sfgdi/controllers/SetterInjectedControllerTest.java
@@ -0,0 +1,25 @@
+package guru.springframework.sfgdi.controllers;
+
+import guru.springframework.sfgdi.services.GreetingServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SetterInjectedControllerTest {
+
+    SetterInjectedController controller;
+
+    // instantiate controller object / use setter to inject GreetingServiceImpl
+    @BeforeEach
+    void setUp() {
+        controller = new SetterInjectedController();
+        controller.setGreetingService(new GreetingServiceImpl());
+    }
+
+    @Test
+    void getGreeting() {
+        System.out.println(controller.getGreeting());
+
+    }
+}
